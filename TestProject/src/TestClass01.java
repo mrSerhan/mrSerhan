1f131ee1d55d4e2d883d0ac7291d72c493b1ebf5
mrSerhan <mrserhan@hotmail.com>
2020-03-14 21:14:02 -0500

    create test02.java

---
+++ TestProject/src/TestClass01.java
@@ -0,0 +1,23 @@
+
+public class TestClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = new String ("serhan");
+		String s2 = new String ("serhan");
+		
+		if (s1 == s2) {
+			System.out.println("true");
+		}else {
+			System.out.println("false");
+		}
+		
+		if (s1.equals(s2)) {
+			System.out.println("true");
+		}else {
+			System.out.println("false");
+		}
+	}
+
+}
